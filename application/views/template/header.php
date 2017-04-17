c9e8e6093c7b568d049c7271d34538d017cb1b92
Campeonato Nassau <jeffersonsilvar.lima@gmail.com>
2017-04-16 23:23:20 -0300

    Versão com dois Botões leia Mais

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -8,7 +8,7 @@
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,</p>
                 <button class="btn btn-lg btn-primary">Buy Now</button>
                 <button class="btn btn-lg btn-success">LEIA MAIS</button>
-                <button class="btn btn-lg btn-success">LEIA MAIS</button>
+                
             </div>
             <div class="col-lg-6 col-md-6 wow bounceInRight">
                 <img  src="<?php echo base_url(); ?>assets/images/Responsive-Web-Design.png" alt="">
