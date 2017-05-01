38bc56cc14a687b000331899b717381ef9301cad
Jefferson Silva <jeffersonsilvar.lima@gmail.com>
2017-05-01 00:45:37 -0300

    Versão com redes Socias

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -4,11 +4,11 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-4 col-md-4">
-                <h4 class="wow fadeInUp">Contact Us</h4>
-                <p><i class="fa fa-home" aria-hidden="true"></i> 111 Main Street, Washington DC, 22222</p>
+                <h4 class="wow fadeInUp">Contato BR</h4>
+                <p><i class="fa fa-home" aria-hidden="true"></i> Rua Guilherme Pinto, 400, Graças - Recife</p>
                 <p><i class="fa fa-envelope" aria-hidden="true"></i> info@media.com</p>
-                <p><i class="fa fa-phone" aria-hidden="true"></i> +1 222 222 2222</p>
-                <p><i class="fa fa-globe" aria-hidden="true"></i> www.media.com</p>
+                <p><i class="fa fa-phone" aria-hidden="true"></i> +55 81 3413 4611</p>
+                <p><i class="fa fa-globe" aria-hidden="true"><a href="http://www.uninassau.edu.br"></i> uninassau.edu.br</a></p>
             </div>
             <div class="col-lg-4 col-md-4">
                 <h4>About</h4>
@@ -17,14 +17,13 @@
                 <p><i class="fa fa-square-o" aria-hidden="true"></i> Term & Condition</p>
             </div>
             <div class="col-lg-4 col-md-4">
-                <h4>Stay in touch</h4>
-                <i class="social fa fa-facebook" aria-hidden="true"></i>
-                <i class="social fa fa-twitter" aria-hidden="true"></i>
-                <i class="social fa fa-linkedin" aria-hidden="true"></i>
-                <i class="social fa fa-pinterest" aria-hidden="true"></i>
-                <i class="social fa fa-youtube" aria-hidden="true"></i>
-                <i class="social fa fa-github" aria-hidden="true"></i><br>
-                <input type="email" placeholder="Subscribe For Updates"><button class="btn btn-success">Subscribe</button>
+                <h4>Midias Sociais</h4>
+                <a href="https://www.facebook.com/UNINASSAU/"><i class="social fa fa-facebook" aria-hidden="true"></i></a>
+                <a href="https://twitter.com/_UNINASSAU"><i class="social fa fa-twitter" aria-hidden="true"></i></a>
+                <a href="https://br.linkedin.com/edu/uninassau-362024"><i class="social fa fa-linkedin" aria-hidden="true"></i></a>
+                <a href="https://www.instagram.com/uninassau/"><i class="social fa fa-instagram" aria-hidden="true"></i></a>
+                <a href="https://www.youtube.com/user/uninassau"><i class="social fa fa-youtube" aria-hidden="true"></i></a>
+
             </div>
         </div>
     </div>
